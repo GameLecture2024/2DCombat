003e4467e1a9d5317c3807d2b7dc6caf69ba99f1
GameLecture2024 <158019948+GameLecture2024@users.noreply.github.com>
2025-02-05 17:50:06 +0900

    (2025.02.05) 스킬 시스템 구현 및 Swap 시스템

---
+++ Assets/Scripts/Slot/Item.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="skill", menuName ="Item/skill")]
+public class Item : ScriptableObject
+{
+    public string itemName;
+    public Sprite iconImage;
+    // Skill 커스텀 클래스 SpellStartegy
+}
